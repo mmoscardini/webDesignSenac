842bc5ac0ac9ef9337c32a5a46d36193e02e3068
Matheus Moscardini <matheus_moscardini@yahoo.com.br>
2016-11-16 19:22:08 -0200

    melhorias na interface e nas imagens

---
+++ bootstrap_version/docs/examples/carousel/form.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <link rel="icon" href="../../favicon.ico">
+      <title>BOOTSTRAP - SERVIÇOS TERCEIRA IDADE</title>
+      <!-- Bootstrap core CSS -->
+      <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
+      <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+      <link href="../../assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+      <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+      <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+      <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
+      <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+      <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+      <![endif]-->
+      <!-- Custom styles for this template -->
+      <link href="carousel.css" rel="stylesheet">
+      <style type="text/css">.\31 2u{width:100%}.\31 1u{width:91.6666666667%}.\31 0u{width:83.3333333333%}.\39 u{width:75%}.\38 u{width:66.6666666667%}.\37 u{width:58.3333333333%}.\36 u{width:50%}.\35 u{width:41.6666666667%}.\34 u{width:33.3333333333%}.\33 u{width:25%}.\32 u{width:16.6666666667%}.\31 u{width:8.3333333333%}.\31 u,.\32 u,.\33 u,.\34 u,.\35 u,.\36 u,.\37 u,.\38 u,.\39 u,.\31 0u,.\31 1u,.\31 2u{float:left;-moz-box-sizing:border-box;-webkit-box-sizing:border-box;-o-box-sizing:border-box;-ms-box-sizing:border-box;box-sizing:border-box}.\-11u{margin-left:91.6666666667%}.\-10u{margin-left:83.3333333333%}.\-9u{margin-left:75%}.\-8u{margin-left:66.6666666667%}.\-7u{margin-left:58.3333333333%}.\-6u{margin-left:50%}.\-5u{margin-left:41.6666666667%}.\-4u{margin-left:33.3333333333%}.\-3u{margin-left:25%}.\-2u{margin-left:16.6666666667%}.\-1u{margin-left:8.3333333333%}.row.flush{margin-left:0}.row.flush>*{padding:0!important}</style>
+   </head>
+   <!-- NAVBAR
+      ================================================== -->
+   <body>
+      <div class="navbar-wrapper">
+         <div class="container">
+            <nav class="navbar navbar-inverse navbar-static-top">
+               <div class="container">
+                  <div class="navbar-header">
+                     <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                     <span class="sr-only">Toggle navigation</span>
+                     <span class="icon-bar"></span>
+                     <span class="icon-bar"></span>
+                     <span class="icon-bar"></span>
+                     </button>
+                     <a class="navbar-brand" href="#">SERVIÇOS TERCEIRA IDADE</a>
+                  </div>
+                  <div id="navbar" class="navbar-collapse collapse">
+                     <ul class="nav navbar-nav">
+                        <li><a href="#about"><strong>LOGIN</strong></a></li>
+                        <li><a href="#contact"><strong>DIVULGUE SEUS SERVIÇOS</strong></a></li>
+                     </ul>
+                  </div>
+               </div>
+            </nav>
+         </div>
+      </div>
+      <!-- Carousel
+         ================================================== -->
+      <div id="myCarousel" class="carousel slide" data-ride="carousel">
+         <div class="carousel-inner" role="listbox">
+            <div class="item active">
+               <img class="first-slide" src="http://placehold.it/1350x500" alt="First slide">
+			   <div class="container">
+		 <div class="carousel-caption">
+            <h3>Encontre um serviço</h3>
+            <select name="Profissoes" id="comboBox" class="selectpicker" style="color: #323233; border-radius: 6px;">
+               <option value="">Selecione</option>
+               <option value="Exercício físico">Exercício físico</option>
+               <option value="Turismo">Turismo</option>
+               <option value="Saúde">Saúde</option>
+               <option value="Educação">Educação</option>
+            </select><br>
+		<footer>
+                           <a href="http://esite.io/store/Df99c2b95e686878ea8f16e465d56047e/site/index.html#" class="btn btn-lg btn-primary">BUSCAR</a>
+                        </footer>
+         </div>
+		 </div>
+            </div>
+         </div>
+         <!-- /input-group -->
+      </div>
+      <!-- /.col-lg-6 -->
+      </div><!-- /.row -->
+      <!-- /.carousel -->
+	  
+      <!-- Marketing messaging and featurettes
+         ================================================== -->
+      <!-- Wrap the rest of the page in another container to center all the content. -->
+      <div class="container marketing">
+         <!-- Three columns of text below the carousel -->
+         <section class="container box-feature1">
+            <div class="row">
+               <div class="12u">
+                  <!--  CLASSE QUE CRIA LINHAS DIVISÓRIAS-->
+                  <header class="first major">
+                  </header>
+                  <h2>COMO FUNCIONA</h2>
+                  <span class="byline">Encontrar profissionais qualificados e focados na terceira idade agora é fácil</span>
+               </div>
+         </section>
+         <div class="col-lg-4">
+         <img  class="img-circle" src="C:/Users/jean.pereira/Desktop/webDesignSenac-master/webDesignSenac-master/Prot+%C2%A6tipo%20alta%20fidelidade/Web%20Design_files/pic01.jpg" alt="Generic placeholder image" width="140" height="140">
+         <h2>Busque por profissionais</h2>
+         <p>Responda algumas perguntas sobre os serviços que você deseja contratar e nós indicamos os profissionais mais adequados para você</p>
+         <!--<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>-->
+         </div><!-- /.col-lg-4 -->
+         <div class="col-lg-4">
+         <img class="img-circle" src="C:/Users/jean.pereira/Desktop/webDesignSenac-master/webDesignSenac-master/Prot+%C2%A6tipo%20alta%20fidelidade/Web%20Design_files/pic02.jpg" alt="Generic placeholder image" width="140" height="140">
+         <h2>Encontre e compare</h2>
+         <p>Analise os profissionais cadastrados em nossa base de dados e solicite orçamentos</p>
+         <!--<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>-->
+         </div><!-- /.col-lg-4 -->
+         <div class="col-lg-4">
+         <img class="img-circle" src="C:/Users/jean.pereira/Desktop/webDesignSenac-master/webDesignSenac-master/Prot+%C2%A6tipo%20alta%20fidelidade/Web%20Design_files/pic03.jpg" alt="Generic placeholder image" width="140" height="140">
+         <h2>Contrate</h2>
+         <p>Compare orçamentos e agende uma data e horario que melhor lhe atenda. Feche negócio pela própria plataforma</p>
+         <!--<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>-->
+         </div><!-- /.col-lg-4 -->
+         </div><!-- /.row -->
+         <section class="container box-feature1">
+            <div class="row">
+               <div class="12u">
+                  <header class="first major">   
+                  </header>
+                  <h2>COMECE A  USAR</h2>
+                  <span class="byline">Encontre profissionais qualificados e com foco em atender a terceira idade</span>
+               </div>
+            </div>
+         </section>
+         <div class="main-wrapper-style1">
+            <div class="inner">
+               <!-- Feature 2 -->
+               <center>
+                  <div class="row">
+                     <div class="col-md-6">
+                        <span class=""><img class="img-circle-style1" src="C:/Users/jean.pereira/Desktop/webDesignSenac-master/webDesignSenac-master/Prot+¦tipo%20alta%20fidelidade/img/ilust_alongamento.jpg" alt=""></span><br>
+                        <footer>
+                           <a href="http://esite.io/store/Df99c2b95e686878ea8f16e465d56047e/site/index.html#" class="btn btn-lg btn-primary">Busque por profissionais</a>
+                        </footer>
+                     </div>
+                     <div class="col-md-6">
+                        <span class=""><img class="img-circle-style1" src="C:/Users/jean.pereira/Desktop/webDesignSenac-master/webDesignSenac-master/Prot+¦tipo%20alta%20fidelidade/img/ilust_anounceservices.jpg" alt=""></span>
+                        <footer>
+                           <a href="http://esite.io/store/Df99c2b95e686878ea8f16e465d56047e/site/index.html#" class="btn btn-lg btn-primary">Anuncie seus serviços</a>
+                        </footer>
+                     </div>
+                  </div>
+               </center>
+            </div>
+         </div>
+         <hr class="featurette-divider">
+         <!-- FOOTER -->
+         <footer>
+            <p class="pull-right"><a href="#">Back to top</a></p>
+            <p>&copy; 2016 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+         </footer>
+      </div>
+      <!-- /.container -->
+      <!-- Bootstrap core JavaScript
+         ================================================== -->
+      <!-- Placed at the end of the document so the pages load faster -->
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+      <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+      <script src="../../dist/js/bootstrap.min.js"></script>
+      <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
+      <script src="../../assets/js/vendor/holder.min.js"></script>
+      <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+      <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+   </body>
+</html>
